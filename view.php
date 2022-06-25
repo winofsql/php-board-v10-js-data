8ff447e4f7a99de584890385a855cc0af34b1e68
sworc <lightbox@nifty.com>
2022-06-25 16:32:25 +0900

    Add files via upload

--- view.php
+++ view.php
@@ -11,6 +11,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.js"></script>
+    <link id="link" rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/base/jquery-ui.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 
 
 
@@ -107,5 +109,8 @@ $( function(){
 
 <iframe id="extend" src="control.php?page=init" name="myframe"></iframe>
 
+<div id="dialog-message" style='display:none;'>
+削除してもよろしいですか?
+</div>
 </body>
 </html>
